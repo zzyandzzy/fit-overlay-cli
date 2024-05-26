62dee6dec03726ea4afc93ce89c91f3bb1ae0be9
Intent <zzy.main@gmail.com>
2024-05-26 12:37:45 +0800

    fear: Update ffmpeg-sidecar version.

--- src/errors.rs
+++ src/errors.rs
@@ -3,8 +3,8 @@ pub enum Error {
     #[error("IO error, {0:?}")]
     IOError(#[from] std::io::Error),
 
-    #[error("FFmpeg error, {0:?}")]
-    FFmpegError(#[from] ffmpeg_sidecar::error::Error),
+    // #[error("FFmpeg error, {0:?}")]
+    // FFmpegError(#[from] ffmpeg_sidecar::error::Error),
 
     #[error(transparent)]
     Other(#[from] anyhow::Error),
